50735f75dcd77a8b9b5725c81a18603e32d08100
Vitalii Aleksandrov <vitaliy0508@ukr.net>
2022-11-16 00:32:44 +0200

    add issue fix with data-save in localStorage

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -6,14 +6,15 @@ import ContactList from 'components/ContactList';
 import Filter from 'components/Filter';
 
 export const App = () => {
-  const [contacts, setContacts] = useState([]);
-
+  const [contacts, setContacts] = useState(
+    JSON.parse(localStorage.getItem('contacts')) ?? [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ]
+  );
   const [filter, setFilter] = useState('');
-
- useEffect(() => {
-    localStorage.setItem('contacts', JSON.stringify(contacts));
-    setContacts(contacts);
-  }, [contacts]);
 
   useEffect(() => {
     const contacts = localStorage.getItem('contacts');
@@ -23,6 +24,10 @@ export const App = () => {
       setContacts(parsedContacts);
     }
   }, []);
+
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
 
   const formSubmitHandler = data => {
     const contact = {
@@ -67,8 +72,3 @@ export const App = () => {
     </div>
   );
 };
-
-// { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-// { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-// { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-// { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
